445cffb7badd74762e5536f4de17ae4db24bcdcd
Kenzo <kenzo.spaulding@gmail.com>
2021-10-26 18:49:44 -0700

    Updated Naming And Formatting

--- src/main/java/edu/uci/banerjee/burnserver/BurnServerApplication.java
+++ src/main/java/edu/uci/banerjee/burnserver/BurnServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BurnServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BurnServerApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(BurnServerApplication.class, args);
+  }
 }
--- src/test/java/edu/uci/banerjee/burnserver/model/FiresRepoTest.java
+++ src/test/java/edu/uci/banerjee/burnserver/model/FiresRepoTest.java
@@ -10,88 +10,98 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-
 @DataJpaTest
 public class FiresRepoTest {
-    @Autowired
-    private FiresRepo repo;
+  @Autowired private FiresRepo repo;
 
-    Fires fire;
-    @BeforeEach
-    void initialize()throws Exception{
-        fire = new Fires(1,0.05,"Hand Pile","Modesto",new SimpleDateFormat("dd/MM/yyyy").parse("12/10/2010"),38.90,-121.05,"Camp","CALFIRE",2010);
-        repo.save(fire);
-    }
+  private Fire fire;
 
-    public FiresRepoTest(){
-    }
+  @BeforeEach
+  void initialize() throws Exception {
+    fire =
+        new Fire(
+            0.05,
+            "Hand Pile",
+            "Modesto",
+            new SimpleDateFormat("dd/MM/yyyy").parse("12/10/2010"),
+            38.90,
+            -121.05,
+            "Camp",
+            "CALFIRE",
+            2010);
+    repo.save(fire);
+  }
 
-    @Test
-    public void save() {
-        Assertions.assertThat(fire.getId()).isGreaterThan(0);
-    }
+  public FiresRepoTest() {}
 
-    @Test
-    void findAll(){
+  @Test
+  public void save() {
+    Assertions.assertThat(fire.getId()).isGreaterThan(0);
+  }
 
-        List<Fires> firesList = repo.findAll();
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findAll() {
+    List<Fire> firesList = repo.findAll();
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findBySource() {
-        List<Fires> firesList = repo.findBySource("CALFIRE");
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findBySource() {
+    List<Fire> firesList = repo.findBySource("CALFIRE");
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByCounty() {
-        List<Fires> firesList = repo.findByCounty("Modesto");
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByCounty() {
+    List<Fire> firesList = repo.findByCounty("Modesto");
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByBurntype() {
-        List<Fires> firesList = repo.findByBurnType("Hand Pile");
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByBurnType() {
+    List<Fire> firesList = repo.findByBurnType("Hand Pile");
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByYear() {
-        List<Fires> firesList = repo.findByYear(2010);
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByYear() {
+    List<Fire> firesList = repo.findByYear(2010);
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByYearIsBetween() {
-        List<Fires> firesList = repo.findByYearIsBetween(2000,2020);
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByYearIsBetween() {
+    List<Fire> firesList = repo.findByYearIsBetween(2000, 2020);
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByAcres() {
-        List<Fires> firesList = repo.findByAcres(0.05);
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByAcres() {
+    List<Fire> firesList = repo.findByAcres(0.05);
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByAcresIsBetween(){
-        List<Fires> firesList =  repo.findByAcresIsBetween(0.0,20.0);
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByAcresIsBetween() {
+    List<Fire> firesList = repo.findByAcresIsBetween(0.0, 20.0);
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 
-    @Test
-    void findByDateIsBetween() throws ParseException {
-        List<Fires> firesList =  repo.findByDateIsBetween(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2000"),new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2011"));
-        Assertions.assertThat(firesList).isNotNull();
-        Assertions.assertThat(firesList.size()).isGreaterThan(0);
-    }
+  @Test
+  void findByDateIsBetween() throws ParseException {
+    List<Fire> firesList =
+        repo.findByDateIsBetween(
+            new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2000"),
+            new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2011"));
+    Assertions.assertThat(firesList).isNotNull();
+    Assertions.assertThat(firesList.size()).isGreaterThan(0);
+  }
 }
