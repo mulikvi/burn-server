c05e219bdde231ee9cc7a31b9f058d014faa7ba2
Kenzo <kenzo.spaulding@gmail.com>
2021-11-01 22:57:44 -0700

    Owner Filter & Refactoring

--- src/main/java/edu/uci/banerjee/burnserver/model/Fire.java
+++ src/main/java/edu/uci/banerjee/burnserver/model/Fire.java
@@ -1,6 +1,9 @@
 package edu.uci.banerjee.burnserver.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -10,52 +13,69 @@ import java.util.Date;
 @Setter
 @ToString
 @NoArgsConstructor
-@Table(name = "fires", indexes = {@Index(columnList = "source", name = "sourceIndex"),
-        @Index(columnList = "county", name = "countyIndex"),
-        @Index(columnList = "year", name = "yearIndex")})
+@Table(
+    name = "fires",
+    indexes = {
+      @Index(columnList = "source", name = "sourceIndex"),
+      @Index(columnList = "county", name = "countyIndex"),
+      @Index(columnList = "year", name = "yearIndex")
+    })
 public class Fire {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, unique = true)
+  private int id;
 
-    @Column(name = "acres")
-    private double acres;
+  @Column(name = "acres")
+  private double acres;
 
-    @Column(name = "burnType")
-    private String burnType;
+  @Column(name = "burnType")
+  private String burnType;
 
-    @Column(name = "county")
-    private String county;
+  @Column(name = "county")
+  private String county;
 
-    @Column(name = "date")
-    private Date date;
+  @Column(name = "date")
+  private Date date;
 
-    @Column(name = "latitude")
-    private double latitude;
+  @Column(name = "latitude")
+  private double latitude;
 
-    @Column(name = "longitude")
-    private double longitude;
+  @Column(name = "longitude")
+  private double longitude;
 
-    @Column(name = "name")
-    private String name;
+  @Column(name = "name")
+  private String name;
 
-    @Column(name = "source")
-    private String source;
+  @Column(name = "source")
+  private String source;
 
-    @Column(name = "year")
-    private int year;
+  @Column(name = "year")
+  private int year;
 
-    public Fire(double acres, String burnType, String county, Date date, double latitude, double longitude, String name, String source, int year) {
-        this.acres = acres;
-        this.burnType = burnType;
-        this.county = county;
-        this.date = date;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.name = name;
-        this.source = source;
-        this.year = year;
-    }
+  @Column(name = "owner")
+  private String owner;
 
+  public Fire(
+      double acres,
+      String burnType,
+      String county,
+      Date date,
+      double latitude,
+      double longitude,
+      String name,
+      String source,
+      int year,
+      String owner) {
+    this.acres = acres;
+    this.burnType = burnType;
+    this.county = county;
+    this.date = date;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.name = name;
+    this.source = source;
+    this.year = year;
+    this.owner = owner;
+  }
 }
---
+++ src/main/java/edu/uci/banerjee/burnserver/services/DataIngestService.java
@@ -0,0 +1,53 @@
+package edu.uci.banerjee.burnserver.services;
+
+import com.univocity.parsers.common.record.Record;
+import edu.uci.banerjee.burnserver.model.Fire;
+import edu.uci.banerjee.burnserver.model.FiresRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+@Service
+@Slf4j
+public class DataIngestService {
+  private final FiresRepo repo;
+  private final LandOwnershipService landOwnershipService;
+
+  public DataIngestService(FiresRepo repo, LandOwnershipService landOwnershipService) {
+    this.repo = repo;
+    this.landOwnershipService = landOwnershipService;
+  }
+
+  public int saveFires(List<Record> records) {
+    log.debug("Saving new fires records.");
+    final var burns = records.parallelStream().map(this::createFire).collect(toUnmodifiableList());
+    repo.saveAll(burns);
+
+    return burns.size();
+  }
+
+  private Fire createFire(final Record fireRecord) {
+    final Fire fire = new Fire();
+    fire.setYear(Integer.parseInt(fireRecord.getString("year")));
+    try {
+      fire.setDate(new SimpleDateFormat("dd/MM/yyyy").parse(fireRecord.getString("date")));
+    } catch (ParseException e) {
+      log.warn("Date is Invalid.");
+    }
+    fire.setName(fireRecord.getString("name"));
+    fire.setAcres(Double.parseDouble(fireRecord.getString("acres")));
+    fire.setLatitude(Double.parseDouble(fireRecord.getString("latitude")));
+    fire.setLongitude(Double.parseDouble(fireRecord.getString("longitude")));
+    fire.setBurnType(fireRecord.getString("burn_type"));
+    fire.setCounty(fireRecord.getString("county"));
+    fire.setSource(fireRecord.getString("source"));
+    fire.setOwner(
+        landOwnershipService.getOwnershipFromCoordinate(fire.getLatitude(), fire.getLongitude()));
+    return fire;
+  }
+}
