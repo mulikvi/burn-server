89be114254d6b9a1dd1793971fee19dca08c9ba4
smk-dev20 <smk.dev20@gmail.com>
2021-11-23 22:08:05 -0800

    add escapes to statistics query, update queries to handle multiple county names

--- src/main/java/edu/uci/banerjee/burnserver/model/FiresRepo.java
+++ src/main/java/edu/uci/banerjee/burnserver/model/FiresRepo.java
@@ -37,7 +37,7 @@ public interface FiresRepo extends JpaRepository<Fire, Integer> {
   // todo this needs some work done to how the start and end months were implemented. We need to
   // revisit the ux of that filter
   @Query(
-      "SELECT f FROM Fire f WHERE (:source is null or f.source = :source) and (:county is null or f.county = :county) and (:countyUnitId is null or f.countyUnitId = :countyUnitId) and"
+      "SELECT f FROM Fire f WHERE (:source is null or f.source = :source) and (:county is null or f.county like %:county%) and (:countyUnitId is null or f.countyUnitId = :countyUnitId) and"
           + "(:minAcres is null or f.acres >= :minAcres) and (:maxAcres is null or f.acres < :maxAcres) and "
           + "(:burnType is null or f.burnType = :burnType) and (:treatmentType is null or f.treatmentType = :treatmentType) and (:startYear is null or f.year >= :startYear) and "
           + "(:endYear is null or f.year >= :endYear) and (:startMonth is null or f.month >= :startMonth) and "
@@ -58,17 +58,19 @@ public interface FiresRepo extends JpaRepository<Fire, Integer> {
       @Param("escaped") Boolean escaped);
 
   @Query(
-      "SELECT COUNT(f), AVG(f.acres), MIN(f.year), MAX(f.year), MIN(f.acres), MAX(f.acres) FROM Fire f WHERE (:source is null or f.source = :source) and (:county is null or f.county = :county) and"
+      "SELECT COUNT(f), AVG(f.acres), MIN(f.year), MAX(f.year), MIN(f.acres), MAX(f.acres) FROM Fire f WHERE (:source is null or f.source = :source) and (:county is null or f.county like %:county%) and (:countyUnitId is null or f.countyUnitId = :countyUnitId) and"
           + "(:minAcres is null or f.acres >= :minAcres) and (:maxAcres is null or f.acres < :maxAcres) and "
-          + "(:burnType is null or f.burnType = :burnType) and (:startYear is null or f.year >= :startYear) and "
+          + "(:burnType is null or f.burnType = :burnType) and (:treatmentType is null or f.treatmentType = :treatmentType) and (:startYear is null or f.year >= :startYear) and "
           + "(:endYear is null or f.year >= :endYear) and (:startMonth is null or f.month >= :startMonth) and "
           + "(:endMonth is null or f.month >= :endMonth) and (:owner is null or f.owner = :owner) and (:escaped is null or f.escaped = :escaped)")
   String filterStatistics(
       @Param("source") String source,
+      @Param("countyUnitId") String countyUnitId,
       @Param("county") String county,
       @Param("minAcres") Double minAcres,
       @Param("maxAcres") Double maxAcres,
       @Param("burnType") String burnType,
+      @Param("treatmentType") String treatmentType,
       @Param("startYear") Integer startYear,
       @Param("endYear") Integer endYear,
       @Param("startMonth") Integer startMonth,
